63e9dff69dc568b50b8acaf0ab683b548acd6efc
Venyx <127613527+VenyxScript@users.noreply.github.com>
2023-03-24 22:26:49 -0400

    Create 7952502098.lua

---
+++ 7952502098.lua
@@ -0,0 +1,25 @@
+-- (Discord - Impossible Glass Bridge Obby)
+local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+
+local Window = DiscordLib:Window("Discord - Impossible Glass Bridge Obby (Squid Game)")
+
+local server1 = Window:Server("Script", "")
+
+local channel1 = server1:Channel("Bridge")
+
+channel1:Seperator()
+
+channel1:Button("Show Correct Path", function()
+for i,v in pairs(game:GetService("Workspace")["Glass Bridge"].GlassPane:GetChildren()) do
+    if v:GetChildren()[1]:FindFirstChild("TouchInterest") == nil and v:GetChildren()[1].Transparency ~= 1 then
+        v:GetChildren()[1].Color = Color3.fromRGB(0,255,0)
+        v:GetChildren()[2].Color = Color3.fromRGB(255,0,0)
+    else
+        v:GetChildren()[2].Color = Color3.fromRGB(0,255,0)
+        v:GetChildren()[1].Color = Color3.fromRGB(255,0,0)
+    end
+end
+DiscordLib:Notification("Discord", "Correct Bridge Is Green, Incorrect Is Red!", "Alright!")
+end)
+
+channel1:Seperator()
